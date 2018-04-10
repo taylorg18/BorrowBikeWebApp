74300a1ed5fea98ded4b2b510889b9e4fb8417ab
Grayson Taylor <taylorg18@up.edu>
2018-04-09 20:47:27 -0700

    Incorporate Database in GridView on bikes Change issue with both Report and Rental being the same as the About class.

--- BorrowBikeWebApp/Rental.aspx.cs
+++ BorrowBikeWebApp/Rental.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,11 +8,28 @@ using System.Web.UI.WebControls;
 
 namespace BorrowBikeWebApp
 {
-    public partial class About : Page
+    public partial class Rental : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+           
+        }
 
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                int myCol = 4;
+                DataRowView _row = (DataRowView)e.Row.DataItem;
+                if (_row != null)
+                {
+                    if ((Boolean)_row.Row["CheckedIn"])
+                        e.Row.Cells[myCol].BackColor = System.Drawing.ColorTranslator.FromHtml("#00FF00");
+                    else
+                        e.Row.Cells[myCol].BackColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+                }
+
+            }
         }
     }
 }
